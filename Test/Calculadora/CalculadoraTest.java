7e002299e29ab3697fb5fde673fdc840d880b967
David <davidleomendoza300@gmail.com>
2023-10-08 15:16:27 -0500

    update test

--- Test/Calculadora/CalculadoraTest.java
+++ Test/Calculadora/CalculadoraTest.java
@@ -2,14 +2,15 @@ package Calculadora;
 
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculadoraTest {
 
-       @AfterClass
-        
+       @BeforeClass
+
         @Test
         public void sumaTest(){
             Calculadora calculadora = new Calculadora(3,4);
